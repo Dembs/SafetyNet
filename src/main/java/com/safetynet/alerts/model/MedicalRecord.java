88ee0d47cd3cab62407d2d76b5b776dfe7a71850
Dembs <demba.sy@hotmail.com>
2024-10-26 22:03:30 +0200

    Creation des models

---
+++ src/main/java/com/safetynet/alerts/model/MedicalRecord.java
@@ -0,0 +1,15 @@
+package com.safetynet.alerts.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MedicalRecord {
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private List<String> medications;
+    private List<String> allergies;
+
+}
